f036d7669c98ef8a84d2864c29cd213bd0a9e0c3
Romanna Yasinska <r.bren.1991@gmail.com>
2022-05-24 20:31:36 +0300

    upd

--- src/hooks/UseModalForm.js
+++ src/hooks/UseModalForm.js
@@ -1,12 +1,13 @@
-import { useUpdateContactMutation} from "redux/contactsItemSlice";
-import { useGetContactsByIdQuery } from 'redux/contactsItemSlice';
+import { useGetContactsQuery, useUpdateContactMutation} from "redux/contactsItemSlice";
 import { sucsessEdit, errorEdit } from "utils/notification";
 import { useForm } from "react-hook-form";
 import { useEffect } from 'react';
 
 
 export const useModalForm = (onClose, id) => {
-  const { data: contact } = useGetContactsByIdQuery(id);
+  const { data: contacts } = useGetContactsQuery();
+  const contact = contacts.find(cont => cont.id === id);
+  console.log(contact)
   const [updateContact, { isLoading, isSuccess, isError }] = useUpdateContactMutation();
   useEffect(() => {
         isSuccess && sucsessEdit();
@@ -21,7 +22,8 @@ export const useModalForm = (onClose, id) => {
     }
     };
     const onSubmit = async (values) => {
-        await handleUpdateContact(values);
+      await handleUpdateContact(values);
+      console.log(values)
     };
   const { register, handleSubmit, formState: { errors } } = useForm({});
   return {register, handleSubmit, errors, onSubmit, contact, isLoading}
